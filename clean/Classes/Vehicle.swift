c77fb8c58a3592851bbbdaff2192e636d3d7f80d
Gabriel O'Flaherty-Chan <gabriel@shopify.com>
2016-03-28 22:44:24 -0400

    add new assets

---
+++ clean/Classes/Vehicle.swift
@@ -0,0 +1,13 @@
+//
+//  Vehicle.swift
+//  clean
+//
+//  Created by Gabriel O'Flaherty-Chan on 2016-03-28.
+//  Copyright © 2016 Gabrieloc. All rights reserved.
+//
+
+import SceneKit
+
+class Truck : SCNNode {
+
+}
